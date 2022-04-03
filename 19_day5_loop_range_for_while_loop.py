f6d02d2a2f650c50df102fba0e0e7a46a9ea9467
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-04-03 20:19:35 +0545

    changed file name to for while loop

--- 19_day5_loop_range_for_while_loop.py
+++ 19_day5_loop_range_for_while_loop.py
@@ -20,16 +20,16 @@
 # print(f"the average height is : {average_height}")
 
 student_heights = input("input a list of student heights :").split()
-for n in range(0, len(student_heights)):
-    student_heights[n] = int(student_heights[n])
-print(f"The height of student is: {student_heights}")
-total_height = 0
-for height in student_heights:
-    total_height = total_height + height
-print(total_height)
-number_of_students = 0
-for nos in student_heights:
-    number_of_students = number_of_students + 1
-print(number_of_students)
-average_height = round (total_height / number_of_students)
-print(f"The average height is : {average_height}")
+# for n in range(0, len(student_heights)):
+#     student_heights[n] = int(student_heights[n])
+# print(f"The height of student is: {student_heights}")
+# total_height = 0
+# for height in student_heights:
+#     total_height = total_height + height
+# print(total_height)
+# number_of_students = 0
+# for nos in student_heights:
+#     number_of_students = number_of_students + 1
+# print(number_of_students)
+# average_height = round (total_height / number_of_students)
+# print(f"The average height is : {average_height}")
