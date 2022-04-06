09da50c5e3523ba5046527c5d55a3c9904a74904
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-04-06 22:11:58 +0545

    function started

---
+++ 20_day6_python_function&kareland_while_loop.py
@@ -0,0 +1,19 @@
+# Function:
+# why function is needed?????
+# if we write repetition of code in a single block then the 
+# block is called function.
+from multiprocessing.sharedctypes import Value
+
+
+# things to consider while defining user defined function:
+# def function_name():
+#     function body 
+#     return Value 
+
+def wish():
+    print("Hello world this is function.")
+wish()
+wish()
+wish()
+wish()
+wish()
