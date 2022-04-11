7fc562f7e4dca95f0ee6a75d318f73e9a060ad6e
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-04-11 22:20:16 +0545

    upto function variable length argument

--- 20_day6_python_function&kareland_while_loop.py
+++ 20_day6_python_function&kareland_while_loop.py
@@ -76,20 +76,79 @@
 
 
 # 1. Positional Argument --order is important 
+# - if we change the order of argument then result is different
+# def add(a,b):
+#     print(a+b)
 
- def add(a,b):
-     print(a+b)
-     
-add(10,20)
+# add(10,20)
+# add(20,10)
 
 # def sub(a,b):
 #     print("The difference is",a-b)
 
 # sub(20,10)
+# sub(10,20)
 
-# 2. Keyword Arguments:
+# 2. Keyword Arguments: in this if we pass argument with parameter name then it is called keyword argument
+# we can pass argument value by keyword i.e. by parameter name
+# here in keyword argument order isn't important but number of argument must be matched 
 # def f1(a,b):
 #     ....... 
 #     ..... 
 #     f1(a=10, b = 20)
 #     f1(a = 20, b =)
+
+# def wish(name,msg):
+#     print("hello",name,msg)
+# wish(name = "ram", msg="good morning")
+# wish(msg="good afternoon", name = "Hari" )
+
+# def wish(name,msg):
+#     print("Hello", name, msg)
+# wish("rajnish", msg= "good morning") #valid
+# wish("rajnish","good afternoon")    #valid
+# # wish(msg="good night","rajnish")  invalid because positional argument is require at first
+# and keyword argument at second 
+
+# 3. Default Argument -
+# if there is no argument passed then we get the default argument 
+# def wish(name = 'guest'):
+#     print('hello',name,'good morning')
+# wish('ram') here argument is passed
+# wish() here default argument is called 
+
+# another example of same type 
+# def wish(name = 'Guest',age):
+#     print('hello',name,'Good morning and your age is',age)
+# wish('ram',25) this is invalid because the non default argument follows the default argument 
+# here name is the default argument while age is non-default argument so non-default argument 
+# should be before
+
+# def wish(age,name = 'Guest'):
+#     print('hello' ,name, 'Good morning and your age is',age)
+# wish('ram',25) this is also invalid although we get output because this is positional argument but not
+# keyword argument so, 25 need to be before the name 
+
+
+# def wish(age,name = 'Guest'):
+#     print('hello' ,name, 'Good morning and your age is',age)
+# wish(25, 'ram')
+
+
+
+# WAP to calculate using positional, default and keyword argument 
+# def calc(a,b,c,d):
+#     sum = a+b
+#     sub = c-d
+#     mul = a*d
+#     div = d/b
+#     return sum,sub,mul,div
+# t = calc(10,20,30,40)
+# m = calc(a = 10, c = 20, b = 30, d= 40)
+# n = calc(10, 20, c = 30, d= 40) #here 10 and 20 are positional argument and other are default argument
+# print(t)
+# print(m)
+# print(n)
+
+  
+# 4. variable length argument 
