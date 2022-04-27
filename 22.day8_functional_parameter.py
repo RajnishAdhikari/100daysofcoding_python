7b2063467ec4eb4993042e4d513102492658a727
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-04-27 22:48:35 +0545

    function completed

--- 22.day8_functional_parameter.py
+++ 22.day8_functional_parameter.py
@@ -61,16 +61,27 @@
 
 #PROGRAM TO IMPLENENT CAESAR CIPHER 
 
-from art import logo
-print(logo)
-r
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f',
  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text= ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+from art import logo
+print(logo)
+
+
 
 # def encrypt(plain_text, shift_amount):
 #     cipher_text = ""
@@ -99,15 +110,18 @@ shift = int(input("Type the shift number:\n"))
 
 
 # rearranging the code to reduce the repetition 
-def caesar(start_text, shift_amount, cipher_direction):
-    end_text= ""
-    if cipher_direction == "decode":
-            shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
-    print(f"The {cipher_direction}d text is {end_text}")
 
-caesar(start_text=text, shift_amount=shift,cipher_direction=direction)
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
+    shift = shift % 25
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to do again. Otherwise type 'no'. \n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
