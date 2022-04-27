a249647cace11a97fd1b84791c2c74d6de158691
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-04-27 21:44:58 +0545

    decryption done now rearranging the code

--- 22.day8_functional_parameter.py
+++ 22.day8_functional_parameter.py
@@ -60,6 +60,8 @@
 
 
 #PROGRAM TO IMPLENENT CAESAR CIPHER 
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f',
  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z']
@@ -77,4 +79,17 @@ def encrypt(plain_text, shift_amount):
         cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
 
-encrypt(plain_text=text, shift_amount=shift)
+
+
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
+    print(f"The decoded text is {plain_text}")
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount = shift)
+elif direction == "decode":
+    decrypt(cipher_text=text, shift_amount=shift)
