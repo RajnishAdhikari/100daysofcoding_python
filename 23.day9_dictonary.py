940ef13dfe81a1527795cab56eba4b1c199c191c
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-04-30 12:18:55 +0545

    basic of dictonary

---
+++ 23.day9_dictonary.py
@@ -0,0 +1,45 @@
+# Python dictonary 
+
+
+# if we want to represent key value pairs as a single entity then 
+# we should go for dictonary 
+# Syntax
+# {key : value, key: value}
+# eg
+# name: roll 
+# domain : ip_address 
+
+# Properties 
+# 1.in dictonary value can be duplicate but key cannot be duplicate 
+# 2. heterogenous object are allowed for both keys and values
+# eg {'rajnish': 100 , 100 : 7.5}
+# 3.Insertio order not preserve
+# this means indexing and slicing isn't possible in dictonary
+# 4.dictonary are mutable 
+# this means we can make changes anywhere in dictonary
+# 5. Dictonary are dynamic
+# this means we make dictonary of any size we want 
+
+
+# how to create dictonary
+# empty dictonary 
+# d = {}
+# print( type(d))
+
+# or 
+# d = dict()   this is also an empty dictonary
+
+# to insert value in empty dictonary 
+# d[100] = "Hello"
+# d[200] = "world"     # another way given below   
+# d = {300: 'ram', 400: 'shyam'}
+# print(d)
+
+
+# ACCESSING THE ELEMENTS OF DICTONARY WITH ASSOCIATED KEY
+d = {300: 'ram', 400: 'shyam',100:'AAAAAAAAA', 200:'BBBBBB'}
+print(d)
+
+
+
+
