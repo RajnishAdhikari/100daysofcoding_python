c5446b2376dc6f076e4c1928fb7b7da860f83d49
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-05-31 22:40:56 +0545

    sample example with multiple return type

--- 24_day10_function_with_output_and_calculator.py
+++ 24_day10_function_with_output_and_calculator.py
@@ -4,10 +4,23 @@
 #     print(l_name.title())
 # format_name("ram","ADHIKARI")
 
+# def format_name(f_name, l_name):
+#     formatted_f_name = f_name.title()
+#     formatted_l_name = l_name.title()
+#     return "f{formatted_f_name} {formatted_l_name}"
+#     print("This is never printed")  #it is never printed because after return the code wont be executed 
+# print(format_name("ram","ADHIKARI"))
+
+# MULTIPLE RETURN VALUE 
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "you didn't provide valid inputs."
+
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
-    return "f{formatted_f_name} {formatted_l_name}"
-print(format_name("ram","ADHIKARI"))
+    return f"Result: {formatted_f_name} {formatted_l_name}"
+
+print(format_name(input("What is your first name?"),input("What is your last name?")))
+
 
 
