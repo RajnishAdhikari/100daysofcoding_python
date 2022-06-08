e3d08e28a07d5cdb9f91d5b3c013c688b892440a
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-06-08 21:44:24 +0545

    return type completed and now doc string started

--- 24_day10_function_with_output_and_calculator.py
+++ 24_day10_function_with_output_and_calculator.py
@@ -12,18 +12,47 @@
 # print(format_name("ram","ADHIKARI"))
 
 # MULTIPLE RETURN VALUE 
-def format_name(f_name, l_name):
-    if f_name == "" or l_name == "":
-        return "you didn't provide valid inputs."
+# def format_name(f_name, l_name):
+#     if f_name == "" or l_name == "":
+#         return "you didn't provide valid inputs."
 
-    formatted_f_name = f_name.title()
-    formatted_l_name = l_name.title()
-    return f"Result: {formatted_f_name} {formatted_l_name}"
+#     formatted_f_name = f_name.title()
+#     formatted_l_name = l_name.title()
+#     return f"Result: {formatted_f_name} {formatted_l_name}"
 
-print(format_name(input("What is your first name?"),input("What is your last name?")))
+# print(format_name(input("What is your first name?"),input("What is your last name?")))
 
 
 #returning true if it is leap year and returning false if it is not a leap year
 
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return True
+#     else:
+#         return False
+
+# def days_in_month(year, month):
+#     # if month > 12 or month < 1:       this is really not necessary
+#     #     return "Invalid month"
+#     month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
+#     if is_leap(year) and month == 2:
+#         return 29
+#     return month_days[month - 1]
+
+
+# year = int(input("Enter a year:"))
+# month = int(input("Enter a month:"))
+# days = days_in_month(year, month)
+# print(days)
+
+
+
+# Doc strings in python
 
 
