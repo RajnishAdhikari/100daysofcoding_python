1968f55c034895839fdbd48e629409e89bfa0cc7
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-06-11 22:51:53 +0545

    calculation going on

--- 24_day10_function_with_output_and_calculator.py
+++ 24_day10_function_with_output_and_calculator.py
@@ -58,6 +58,43 @@
 
 
 # Calculator Project using function and dictonary 
+
 #add
 def add (n1, n2):
     return n1 + n2
+
+#subtract
+def subtract (n1, n2):
+    return n1 - n2
+
+#multiply
+def multiply (n1, n2):
+    return n1 * n2
+
+#divide
+def divide (n1, n2):
+    return n1 / n2
+
+
+# Now storing the function into the dictonary
+operations = {
+
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+
+}
+
+num1 = int(input("What's the first number?:"))
+
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("What's the second number?:"))
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
