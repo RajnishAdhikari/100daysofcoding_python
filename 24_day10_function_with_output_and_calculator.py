5f6565379cbcfdaacf65a2fa12780c83832ac095
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-06-12 20:29:31 +0545

    calculator project completed

--- 24_day10_function_with_output_and_calculator.py
+++ 24_day10_function_with_output_and_calculator.py
@@ -60,6 +60,9 @@
 # Calculator Project using function and dictonary 
 
 #add
+from turtle import showturtle
+
+
 def add (n1, n2):
     return n1 + n2
 
@@ -86,15 +89,25 @@ operations = {
 
 }
 
-num1 = int(input("What's the first number?:"))
+def calculator():
+    num1 = float(input("What's the first number?:"))
 
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-for symbol in operations:
-    print(symbol)
+    while should_continue:
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = float(input("What's the second number?:"))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number?:"))
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+        if  input(f"Type 'y' to continue calculating with {answer} , or type 'n' to start a new calculation:") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
